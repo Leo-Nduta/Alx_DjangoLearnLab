10af19aec8b0043e6f9ad56e7085adcc23a5d2a4
Leonora Nduta <nduta13mbugua@gmail.com>
2025-11-09 18:01:51 +0300

    "removed library = Library...."

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,8 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 def books_in_library(library_name):
-        library = Library.objects.get(name = library_name)
-        return library.books.all()
+        Library.objects.get(name = library_name)
+        return Library.books.all()
 
 def books_by_author(author_name):
     try:
