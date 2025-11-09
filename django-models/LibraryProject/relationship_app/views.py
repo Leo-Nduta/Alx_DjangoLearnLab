96f1cbfd5b3ed97bc777cb568fdd98d6df85def4
Leonora Nduta <nduta13mbugua@gmail.com>
2025-11-09 19:23:19 +0300

    ".html in views.py"

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,10 +5,10 @@ from django.views import View
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
-    return render(request, Book.title, {'books': books}, Author.name, {'authors': Author.objects.all()})
+    return render(request, "relationship_app.html")
 
 class DetailView(View):
     def get(self, **kwargs):
         library = Library.objects.get(id=kwargs['library_id'])
-        return render(request, 'library_detail.html', {'library': library, 'books': library.books.all()})
+        return render('library_detail.html', {'library': library, 'books': library.books.all()})
 
