3ad614a98e46394e8f526db7658feefd6c5674a7
Leonora Nduta <nduta13mbugua@gmail.com>
2025-11-16 20:44:57 +0300

    moved to bookshelf

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -45,29 +45,6 @@ class Librarian(models.Model):
 # UserProfile for Role-Based Access
 # -----------------
 
-from django.contrib.auth.models import AbstractUser, UserManager
-
-class CustomUserManager(UserManager):
-    def create_user(self, username, email, password, **extra_fields):
-        extra_fields.setdefault('date_of_birth', None)
-        extra_fields.setdefault('profile_photo', None)
-        return super().create_user(username, email, password, **extra_fields)
-
-    def create_superuser(self, username, email, password, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-        return self().create_user(username, email, password, **extra_fields)
-
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=False, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=False, blank=True)
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.username
-
 from django.conf import settings
 
 # Reference the custom user model dynamically
