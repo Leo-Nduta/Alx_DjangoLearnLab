bfc223cc48e689286cda6723707b6e37f72c5352
Leonora Nduta <nduta13mbugua@gmail.com>
2025-11-16 20:39:27 +0300

    AbstractUser commit

---
+++ advanced_features_and_security/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,15 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+def books_in_library(library_name):
+        Library.objects.get(name=library_name)
+        return Library.books.all()
+
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    Book.objects.filter(author=author)
+    return Author.books.all()
+
+def librarian_of_library(library_name):
+        library = Library.objects.get(name=library_name)
+        Librarian.objects.get(library=library)
+        return library.librarian
