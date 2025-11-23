b2e426355f0909f77fd34297bb3aba1de2b1493b
Leonora Nduta <nduta13mbugua@gmail.com>
2025-11-23 15:22:08 +0300

    created api/urls and added the BookList path

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -20,6 +20,6 @@ from api.views import BookList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('api/', include('api.urls')),
+    path('api/', include('api.urls')),
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
 ]
