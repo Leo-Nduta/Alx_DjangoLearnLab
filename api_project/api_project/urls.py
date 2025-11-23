40e343881a6bb3fea5a0a8259fdc1fcb0af235b1
Leonora Nduta <nduta13mbugua@gmail.com>
2025-11-23 19:41:33 +0300

    Commit to confirm whether i should use urls in api or otherwise

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api.views import BookList
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('api/token/', obtain_auth_token, name='api_token_auth')
 ]
