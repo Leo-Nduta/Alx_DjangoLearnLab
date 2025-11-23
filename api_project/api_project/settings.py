af8abe1cd70241225ac9ea9d6189449a42b27a21
Leonora Nduta <nduta13mbugua@gmail.com>
2025-11-23 19:51:38 +0300

    added .permissions.IsAuthenticated to api_project/settings

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -127,6 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',  # optional
+        'rest_framework.authentication.SessionAuthentication',# optional
+        'rest_framework.permissions.IsAuthenticated'
     ]
 }
