3cf174564bb707ae9a2e42d7d5266fd1f0ff959d
Leonora Nduta <nduta13mbugua@gmail.com>
2025-12-07 20:04:17 +0300

    added CRUD options

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from blog import views
 from django.contrib.auth import views as auth_views
+from .views import PostCreateView, PostDetailView, PostListView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +10,8 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('/post/new/', PostCreateView.as_view(), name='create_post'),
+    path('/post/<int:pk>/edit/', PostUpdateView.as_view(), name='update_post'),
+    path('/posts/<int:pk>/delete/', PostDeleteView.as_view(), name='delete_post'),
+    path('/posts/<int:pk>/', PostDetailView.as_view(), name = 'view_post'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -6,6 +6,7 @@ from .models import Post
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.shortcuts import redirect
 
 # Create your views here.
 def register(request):
@@ -36,11 +37,107 @@ def profile(request):
         profile.save()
         return render(request, 'blog/profile.html', {'message': 'Profile updated successfully!'})
 
-    return render(request, 'profile.html'), {'profile' : profile}
+#return render(request, 'profile.html'), {'profile' : profile}
+
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         profile.objects.create(user=instance)
         instance.profile.save()
 
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
 
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "post_create.html"
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = "post_update.html"
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = '/'
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
+
+@login_required
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user      # automatically set logged-in user
+            post.save()
+            return redirect('post_list')     # change name if needed
+    else:
+        form = PostForm()
+
+    return render(request, 'create_post.html', {'form': form})
+
+from django.shortcuts import get_object_or_404
+
+@login_required
+def update_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            updated_post = form.save(commit=False)
+            updated_post.author = request.user   # ensure correct author
+            updated_post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'update_post.html', {'form': form})
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
+from .models import Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']   # author is set automatically
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import UpdateView, ListView, DetailView
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+from django.views.generic import ListView
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+from django.views.generic import DetailView
+
+class PostDetailView(DetailView):
+    model = Post
