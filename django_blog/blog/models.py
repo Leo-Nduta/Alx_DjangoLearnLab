8c783e9d533083e18ff9c7eaeb1923d1f2de4f93
Leonora Nduta <nduta13mbugua@gmail.com>
2025-12-07 17:35:51 +0300

    introduced signals fie

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -13,3 +13,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     profile_pic = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.user.username
