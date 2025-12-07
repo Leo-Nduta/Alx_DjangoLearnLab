585c0ecdc0529896d70ab4c7e0a1ff5b2ce2963f
Leonora Nduta <nduta13mbugua@gmail.com>
2025-12-07 20:05:51 +0300

    changed path code in urls to post instead of posts

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('/post/new/', PostCreateView.as_view(), name='create_post'),
     path('/post/<int:pk>/edit/', PostUpdateView.as_view(), name='update_post'),
-    path('/posts/<int:pk>/delete/', PostDeleteView.as_view(), name='delete_post'),
-    path('/posts/<int:pk>/', PostDetailView.as_view(), name = 'view_post'),
+    path('/post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete_post'),
+    path('/post/<int:pk>/', PostDetailView.as_view(), name = 'view_post'),
 ]
