371b1214145c7419cf4df0242fb90acd2c9a8967
Leonora Nduta <nduta13mbugua@gmail.com>
2025-12-07 20:07:17 +0300

    corrected update url path

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('/post/new/', PostCreateView.as_view(), name='create_post'),
-    path('/post/<int:pk>/edit/', PostUpdateView.as_view(), name='update_post'),
+    path('/post/<int:pk>/update/', PostUpdateView.as_view(), name='update_post'),
     path('/post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete_post'),
     path('/post/<int:pk>/', PostDetailView.as_view(), name = 'view_post'),
 ]
