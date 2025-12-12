5a789613e6e7cdcc831c4d2664c2776c8a98e013
Leonora Nduta <nduta13mbugua@gmail.com>
2025-12-12 16:31:09 +0300

    accounts setup and user authentication

---
+++ social_media_api/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from accounts import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('profile/', views.profile, name='profile'),
+]
