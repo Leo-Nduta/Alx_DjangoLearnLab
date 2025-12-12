45b301a72869bc9a23901cbc064f68283e97b4fc
Leonora Nduta <nduta13mbugua@gmail.com>
2025-12-12 18:49:26 +0300

    added api path to urls

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -15,17 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import PostCreateView, PostDetailView, PostUpdateView, PostDeleteView, CommentCreateView, CommentDetailView, CommentUpdateView, CommentDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/post/new/', PostCreateView.as_view(), name='create_post'),
-    path('/post/<int:pk>/update/', PostUpdateView.as_view(), name='update_post'),
-    path('/post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete_post'),
-    path('/post/<int:pk>/', PostDetailView.as_view(), name = 'view_post'),
-    path('/post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='create_comment'),
-    path('/comment/<int:pk>/update/', CommentUpdateView.as_view(), name='update_comment'),
-    path('/comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete_comment'),
-    path('/comment/<int:pk>/', CommentDetailView.as_view(), name = 'view_comment'),
+    path('api/', include('posts.urls'))
 ]
