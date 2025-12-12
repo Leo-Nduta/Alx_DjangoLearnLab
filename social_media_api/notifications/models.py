2d4a3afbb61c22f81bb93edaff39f8ded07278c0
Leonora Nduta <nduta13mbugua@gmail.com>
2025-12-13 01:36:10 +0300

    adjusted timestamp model

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -6,7 +6,7 @@ class Notification(models.Model):
     actor = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE, related_name='actions')
     verb = models.CharField(max_length=255)
     target = models.GenericForeignKey('content_type', 'object_id')
-    time_stamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return f"Notification for {self.recipient.username}: {self.verb}"
