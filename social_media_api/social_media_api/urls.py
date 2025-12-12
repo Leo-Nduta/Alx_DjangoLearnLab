58c32eb31cdcb3db6555d912e9537eb52d9d17c0
Leonora Nduta <nduta13mbugua@gmail.com>
2025-12-12 18:47:01 +0300

    added posts urls to main project urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import PostCreateView, PostDetailView, PostUpdateView, PostDeleteView, CommentCreateView, CommentDetailView, CommentUpdateView, CommentDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/post/new/', PostCreateView.as_view(), name='create_post'),
+    path('/post/<int:pk>/update/', PostUpdateView.as_view(), name='update_post'),
+    path('/post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete_post'),
+    path('/post/<int:pk>/', PostDetailView.as_view(), name = 'view_post'),
+    path('/post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='create_comment'),
+    path('/comment/<int:pk>/update/', CommentUpdateView.as_view(), name='update_comment'),
+    path('/comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete_comment'),
+    path('/comment/<int:pk>/', CommentDetailView.as_view(), name = 'view_comment'),
 ]
